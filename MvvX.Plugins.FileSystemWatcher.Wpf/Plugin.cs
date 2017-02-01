84f6419a37c7d9bb445053942d8069f2264a8217
Vincent Daude <vincent.daude@econocom.com>
2017-02-01 15:31:26 +0100

    Add Plugin in FileSystemWatcher for Wpf

---
+++ MvvX.Plugins.FileSystemWatcher.Wpf/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Platform;
+using MvvmCross.Platform.Plugins;
+
+namespace MvvX.Plugins.FileSystemWatcher.Wpf
+{
+    class Plugin
+        : IMvxPlugin
+    {
+        public void Load()
+        {
+            Mvx.RegisterSingleton<IFileSystemWatcher>(new PlatformFileSystemWatcher());
+        }
+    }
+}
